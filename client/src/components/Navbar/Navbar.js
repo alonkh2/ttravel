b21b694275e231e9f940a45a33aa39196e5435a5
alonkh2 <alonkh2@gmail.com>
2021-11-30 18:43:24 +0200

    cool stuff

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+import ResponsiveAppBar from '../ResponsiveAppBar' 
+
+function Navbar() {
+    return (
+        <div>
+            <ResponsiveAppBar />
+        </div>
+    )
+}
+
+export default Navbar
